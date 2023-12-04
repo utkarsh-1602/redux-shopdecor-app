3df355852d94ca3de6437ad52d1bbbc5a0876422
utkarsh-1602 <usking619@gmail.com>
2023-12-05 02:27:32 +0530

    files deleted

--- src/redux/reducer/index.js
+++
@@ -1,8 +0,0 @@
-// Combine all reducers together
-
-import { combineReducers } from "redux";
-import { productReducer } from "./productReducer";
-
-const rootReducer = combineReducers({
-    allProducts: productReducer
-});
